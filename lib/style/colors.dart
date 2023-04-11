de92e48e26983c6c3fc28701baf918b99a19da29
kksarang <sarangrajan17@gmail.com>
2023-04-11 11:10:30 +0530

    new

---
+++ lib/style/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color.fromARGB(255, 222, 210, 210);
+  static const secondary = Color.fromARGB(255, 221, 60, 60);
+  static const iconGray = Color.fromARGB(255, 221, 85, 43);
+  static const black = Colors.black;
+  static const primary = Color(0xff262626);
+  static const primaryBg = Color.fromARGB(255, 84, 185, 207);
+  static const secondaryBg = Color.fromARGB(255, 16, 70, 30);
+  static const barBg = Color.fromARGB(255, 171, 171, 239);
+}
